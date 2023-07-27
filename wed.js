9ec1decbf238f0aebb02b98b23bf3b4bd5b9e03b
rishabhrawat05 <rishabhrawat.171105@gmail.com>
2023-07-27 15:56:23 +0530

    first commit

---
+++ wed.js
@@ -0,0 +1,11 @@
+var a=document.querySelector("#nav3 #men");
+var b=document.querySelector("#menu")
+var c=document.querySelector("#menu i")
+a.addEventListener("click",function(){
+    b.style.left="0"
+    
+})
+c.addEventListener("click",function(){
+    b.style.left="-100%"
+    
+})
